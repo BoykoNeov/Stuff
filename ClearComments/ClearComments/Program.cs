0bec4296578e9d55e2abe94a5962e393f351bd61
BoykoNeov <boikoneov@gmail.com>
2017-06-20 23:15:34 +0300

    :angel: Removes all lines starting with at least two '/'  from a file

---
+++ ClearComments/ClearComments/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+/// <summary>
+/// a program that removes all text starting with two or mode '/' and ending with newline from a file
+/// </summary>
+public class OddLines
+{
+    public static void Main(string[] args)
+    {
+        string input = File.ReadAllText(args[0]);
+        string output = string.Empty;
+
+        string regex = @"\/\/.*\n";
+        output = Regex.Replace(input, regex, "");
+
+        File.WriteAllText(args[0], output.ToString());
+    }
+}
