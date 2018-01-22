7e7c6f1087c13c91e69cccdf1bcc7ad405bd0ec0
BoykoNeov <boikoneov@gmail.com>
2018-01-22 23:40:37 +0200

    "fish" Started work on SHA-1 implementation

---
+++ SHA-1/SHA-1/Constants.cs
@@ -0,0 +1,19 @@
+﻿namespace SHA_1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Constants
+    {
+        const string Kt_0to_19 = "5a827999"; // for rounds 0 to 19
+        const string Kt_20to39 = "6ed9eba1"; // for rounds 20 to 39
+        const string Kt_40to59 = "8f1bbcdc"; // for rounds 40 to 59
+        const string Kt_60to79 = "ca62c1d6"; // for rounds 60 to 79
+
+        // Convert.ToUInt32(hex, 16)
+
+    }
+}
---
+++ SHA-1/SHA-1/StartUp.cs
@@ -0,0 +1,47 @@
+﻿namespace SHA_1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            string output = PadToMod512(input);
+            Console.WriteLine(output);
+        }
+
+        public static string PadToMod512(string data)
+        {
+            // Convert input to binary
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in data.ToCharArray())
+            {
+                sb.Append(Convert.ToString(c, 2).PadLeft(8, '0'));
+            }
+
+            string convertedToBinary = sb.ToString();
+            string messageLengthInBinary = Convert.ToString(convertedToBinary.Length, 2).PadLeft(64, '0');
+
+            // Add 1
+            string message = convertedToBinary + "1";
+
+            // Pad to 448 (448 + 64 bits for message length = 512)
+            if (message.Length % 448 > 0)
+            {
+                message = message.PadRight(message.Length + (448 - message.Length % 512)  % 448, '0');
+            }
+
+            int test = message.Length + ((448 - message.Length % 512) % 448);
+
+            message = message + messageLengthInBinary;
+
+            return message + $" message length = {message.Length}, convertedtobinarylengthplus1 = {convertedToBinary.Length + 1}";
+        }
+    }
+}
