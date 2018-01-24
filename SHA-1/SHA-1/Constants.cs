56cb911d015f7f8f77bd2129f29ac0e315b10c2d
BoykoNeov <boikoneov@gmail.com>
2018-01-24 23:28:47 +0200

    :collision: ongoing work on SHA-1

--- SHA-1/SHA-1/Constants.cs
+++ SHA-1/SHA-1/Constants.cs
@@ -1,20 +1,17 @@
 ﻿namespace SHA_1
 {
-    public class Constants
+    internal class Constants
     {
         // initial hash values
-        const string H0 = "67452301";
-        const string H1 = "efcdab89";
-        const string H2 = "98badcfe";
-        const string H3 = "10325476";
-        const string H4 = "c3d2e1f0";
+        internal const uint H0 = 1732584193; // 67452301 in hex
+        internal const uint H1 = 4023233417; // efcdab89 in hex
+        internal const uint H2 = 2562383102; // 98badcfe in hex
+        internal const uint H3 = 271733878; // 10325476 in hex
+        internal const uint H4 = 3285377520; // c3d2e1f0 in hex
 
-        const string Kt_0to_19 = "5a827999"; // for rounds 0 to 19
-        const string Kt_20to39 = "6ed9eba1"; // for rounds 20 to 39
-        const string Kt_40to59 = "8f1bbcdc"; // for rounds 40 to 59
-        const string Kt_60to79 = "ca62c1d6"; // for rounds 60 to 79
-
-        // Convert.ToUInt32(hex, 16)
-
+        internal const uint Kt_0to_19 = 1518500249; // 5a827999 in hex, for rounds 0 to 19
+        internal const uint Kt_20to39 = 1859775393; // 6ed9eba1 in hex, for rounds 20 to 39
+        internal const uint Kt_40to59 = 2400959708; // 8f1bbcdc in hex, for rounds 40 to 59
+        internal const uint Kt_60to79 = 3395469782; // ca62c1d6 in hex for rounds 60 to 79
     }
 }
--- SHA-1/SHA-1/StartUp.cs
+++ SHA-1/SHA-1/StartUp.cs
@@ -13,36 +13,121 @@
             string paddedInput = PadToMod512(input);
             string[] blocks = SplitToChunks(paddedInput, 512).ToArray();
 
-            //TODO foreach block
+            uint[] overallHashes = new uint[5] { Constants.H0, Constants.H1, Constants.H2, Constants.H3, Constants.H4 };
+
             List<uint> words = SplitToChunks(blocks[0], 32).Select(x => Convert.ToUInt32(x, 2)).ToList();
+
+            //TODO foreach block/chunk
+
 
             for (int i = 16; i < 80; i++)
             {
                 // Perform XOR
-                uint currentWord = words[i-3] ^ words[i-8] ^ words[i-14] ^ words[i-16];
+                uint currentWord = words[i - 3] ^ words[i - 8] ^ words[i - 14] ^ words[i - 16];
 
-                // ... which would perform a circular shift of a 32 bit value.
-                // As a generalization to circular shift left n bits, on a b bit variable:
-                /*some unsigned numeric type*/
-                // var result = input << n | input >> (b - n);
-
-                // Rotate left
-                currentWord = currentWord << 1 | currentWord >> 31;
+                // Rotate left by 1
+                currentWord = RotateLeft(currentWord, 1);
                 words.Add(currentWord);
             }
 
+            // initialize working variables with constants
+            uint a = overallHashes[0];
+            uint b = overallHashes[1];
+            uint c = overallHashes[2];
+            uint d = overallHashes[3];
+            uint e = overallHashes[4];
 
+            for (int i = 0; i < 80; i++)
+            {
+                uint constant = 0;
+
+                if (i <= 19)
+                {
+                    constant = Constants.Kt_0to_19;
+                }
+                else if(i <= 39)
+                {
+                    constant = Constants.Kt_20to39;
+                }
+                else if (i <= 59)
+                {
+                    constant = Constants.Kt_40to59;
+                }
+                else
+                {
+                    constant = Constants.Kt_60to79;
+                }
+
+                uint temp = RotateLeft(a, 5) + ProcessingFunction(i, b, c, d) + e + constant + words[i];
+                e = d;
+                d = c;
+                c = RotateLeft(c, 30);
+                b = a;
+                a = temp;
+            }
+
+            // Compute intermidiate hash values
+            overallHashes[0] += a;
+            overallHashes[1] += a;
+            overallHashes[2] += a;
+            overallHashes[3] += a;
+            overallHashes[4] += a;
+
+
+            // final hash
+            string result = string.Join("", overallHashes.Select(x => Convert.ToString(x, 16)));
 
             Console.WriteLine();
         }
 
-        public static IEnumerable<string> SplitToChunks(string str, int chunkSize)
+        // SHA-1 "Processing function"
+        private static uint ProcessingFunction(int round, uint b, uint c, uint d)
+        {
+            uint result = 0;
+
+            if (round <= 19)
+            {
+                result = (b & c) | ((~b) & d);
+            }
+            else if (round <= 39)
+            {
+                result = b ^ c ^ d;
+            }
+            else if (round <= 59)
+            {
+                result = (b & c) | (b & d) | (c & d);
+            }
+            else
+            {
+                result = b ^ c ^ d;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Rotates an uint bitwise by a specified ammount
+        /// </summary>
+        /// <param name="input">uint to be rotated</param>
+        /// <param name="numberOfRotations">positions to rotate</param>
+        /// <returns></returns>
+        // ... which would perform a circular shift of a 32 bit value.
+        // As a generalization to circular shift left n bits, on a b bit variable:
+        /*some unsigned numeric type*/
+        // var result = input << n | input >> (b - n);
+        private static uint RotateLeft(uint input, int numberOfRotations)
+        {
+            return input << numberOfRotations | input >> 32 - numberOfRotations;
+        }
+
+
+        private static IEnumerable<string> SplitToChunks(string str, int chunkSize)
         {
             return Enumerable.Range(0, str.Length / chunkSize)
                 .Select(i => str.Substring(i * chunkSize, chunkSize));
         }
 
-        public static string PadToMod512(string data)
+        private static string PadToMod512(string data)
         {
             // Convert input to binary
             StringBuilder sb = new StringBuilder();
