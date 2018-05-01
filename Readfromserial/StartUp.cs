9eed6f257c0c813e8bd7e52ff49f403d859c16e8
BoykoNeov <boikoneov@gmail.com>
2018-05-01 18:14:21 +0300

    :door: a program to read bytes from serial port and append themto file

---
+++ Readfromserial/StartUp.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+using System.IO;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        const int ArraysToCycle = 200;
+        const int ArraySize = 10_000;
+
+        Console.WriteLine("Enter file path with file name: ");
+        string filePath = Console.ReadLine();
+
+        if (!File.Exists(filePath))
+        {
+            using (var stream = File.Create(filePath))
+            {
+            }
+        }
+
+        var port = new SerialPort("COM5", 9600, Parity.None, 8, StopBits.One);
+        port.Open();
+
+
+        Thread.Sleep(3000);
+
+        for (int j = 0; j < ArraysToCycle; j++)
+        {
+            byte[] bytes = new byte[ArraySize];
+            for (int i = 0; i < ArraySize; i++)
+            {
+                var portInput = port.ReadLine();
+
+                try
+                {
+                    bytes[i] = byte.Parse(portInput);
+                }
+                catch
+                {
+                    Console.WriteLine($"Error parsing: {portInput}");
+                    if (i > 0)
+                    {
+                        i--;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Processed this far: {(j + 1) * ArraySize}");
+            AppendAllBytes(filePath, bytes);
+        }
+
+        port.Close();
+    }
+
+    public static void AppendAllBytes(string path, byte[] bytes)
+    {
+        //argument-checking here.
+
+        using (var stream = new FileStream(path, FileMode.Append))
+        {
+            stream.Write(bytes, 0, bytes.Length);
+        }
+    }
+}
